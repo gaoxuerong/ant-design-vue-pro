d1813e2fb8b2232bdd65619678989d2a45c657bd
gaoxuerong <gaoxuerong123@gmail.com>
2020-06-11 10:46:20 +0800

    .

--- src/views/Forms/BasicForm.vue
+++ src/views/Forms/BasicForm.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>basic form</div>
+  <div>
+    <div>basic form</div>
+    <router-link to="step-form/info">baidu</router-link>
+  </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    console.log(this.$router);
+    console.log(this.$route);
+    console.log(this.$routes);
+    console.log("");
+    this.$router.push({ path: "step-form/info" });
+  }
+};
 </script>
 <style lang="less" scoped></style>
